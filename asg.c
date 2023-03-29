d8cf6e3a50936ba9bbd1422a1f6774a60299c638
Paly <112581464+palyyyy@users.noreply.github.com>
2023-03-29 09:55:00 +0300

    Add files via upload

---
+++ asg.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+void printmenu()
+{
+    printf("---MENU---\n");
+    printf("# n: name\n");
+    printf("# m: last modif\n");
+    printf("# a: acces\n");
+    printf("Please enter your options :\n");
+}
+
+int main(int argc,char *argv[])
+{
+
+    printf("WIP\n");
+    printf("%d", argc - 1);
+}
